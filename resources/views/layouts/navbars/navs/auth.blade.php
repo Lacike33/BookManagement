4823bd2062206d597a141d69d634c0d548d1a275
LV consult s.r.o <ladislav.valient@gmail.com>
2020-01-14 22:38:10 +0100

    Edit Admin pages.

--- resources/views/layouts/navbars/navs/auth.blade.php
+++ resources/views/layouts/navbars/navs/auth.blade.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-lg navbar-transparent navbar-absolute fixed-top ">
   <div class="container-fluid">
     <div class="navbar-wrapper">
-      <a class="navbar-brand" href="#">{{ $titlePage }}</a>
+      <a class="navbar-brand" href="#">{{ __('menu.dashboard') }}</a>
     </div>
     <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
     <span class="sr-only">Toggle navigation</span>
@@ -21,30 +21,30 @@
         </div>
       </form>
       <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="{{ route('home') }}">
-            <i class="material-icons">dashboard</i>
-            <p class="d-lg-none d-md-block">
-              {{ __('Stats') }}
-            </p>
-          </a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="material-icons">notifications</i>
-            <span class="notification">5</span>
-            <p class="d-lg-none d-md-block">
-              {{ __('Some Actions') }}
-            </p>
-          </a>
-          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" href="#">{{ __('Mike John responded to your email') }}</a>
-            <a class="dropdown-item" href="#">{{ __('You have 5 new tasks') }}</a>
-            <a class="dropdown-item" href="#">{{ __('You\'re now friend with Andrew') }}</a>
-            <a class="dropdown-item" href="#">{{ __('Another Notification') }}</a>
-            <a class="dropdown-item" href="#">{{ __('Another One') }}</a>
-          </div>
-        </li>
+{{--        <li class="nav-item">--}}
+{{--          <a class="nav-link" href="{{ route('home') }}">--}}
+{{--            <i class="material-icons">dashboard</i>--}}
+{{--            <p class="d-lg-none d-md-block">--}}
+{{--              {{ __('Stats') }}--}}
+{{--            </p>--}}
+{{--          </a>--}}
+{{--        </li>--}}
+{{--        <li class="nav-item dropdown">--}}
+{{--          <a class="nav-link" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">--}}
+{{--            <i class="material-icons">notifications</i>--}}
+{{--            <span class="notification">5</span>--}}
+{{--            <p class="d-lg-none d-md-block">--}}
+{{--              {{ __('Some Actions') }}--}}
+{{--            </p>--}}
+{{--          </a>--}}
+{{--          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">--}}
+{{--            <a class="dropdown-item" href="#">{{ __('Mike John responded to your email') }}</a>--}}
+{{--            <a class="dropdown-item" href="#">{{ __('You have 5 new tasks') }}</a>--}}
+{{--            <a class="dropdown-item" href="#">{{ __('You\'re now friend with Andrew') }}</a>--}}
+{{--            <a class="dropdown-item" href="#">{{ __('Another Notification') }}</a>--}}
+{{--            <a class="dropdown-item" href="#">{{ __('Another One') }}</a>--}}
+{{--          </div>--}}
+{{--        </li>--}}
         <li class="nav-item dropdown">
           <a class="nav-link" href="#pablo" id="navbarDropdownProfile" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <i class="material-icons">person</i>
@@ -54,7 +54,7 @@
           </a>
           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownProfile">
             <a class="dropdown-item" href="{{ route('profile.edit') }}">{{ __('Profile') }}</a>
-            <a class="dropdown-item" href="#">{{ __('Settings') }}</a>
+{{--            <a class="dropdown-item" href="#">{{ __('Settings') }}</a>--}}
             <div class="dropdown-divider"></div>
             <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">{{ __('Log out') }}</a>
           </div>
